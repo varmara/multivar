5e703419bb68c415dc7b070de1ce290c17a3fbd0
Vadim Khaitov <polydora@rambler.ru>
2019-02-13 16:50:12 +0300

    Обновил презентацю №1

--- 01_multivariate_data_and_dissimilarities_code.R
+++ 01_multivariate_data_and_dissimilarities_code.R
@@ -7,18 +7,11 @@
 
 
 # Читаем данные
-protein <- read.table("data/Protein.txt", header = TRUE, sep = "\t")
-head(protein)
+
+abund <- read.table("data/dolg_abundance.txt", skip = 1, header = TRUE, sep = ";")
+hydrol <- read.table("data/dolg_hydrology.txt", skip = 1, header = TRUE, sep = ";")
 
  # Код для пересчета данных в относительные величины
-
-
-
-
-
-# protein_stand$Country <- protein$Country
-
-row.names(protein_stand) <- protein$Country
 
 
 
@@ -27,34 +20,96 @@ row.names(protein_stand) <- protein$Country
 
 library(vegan)
 
+# Проводим подготовку данных в vegan
+
+
+
+
 
 
 
 # Вычисляем матрицу расстояний между объектами в пакете vegan
 # Заполняем пропущенные куски кода
 
-bray <- vegdist( , method =  )
+dist_init <- vegdist( , method =  )
 
-euclid <- vegdist( , method =  )
+dist_stand <- vegdist( , method =  )
+
+dist_log <- vegdist( , method =  )
+
+dist_rel <- vegdist(, method = )
+
+
+
 
 # Представляем матрицу расстояний в развернутом виде
+# Пользовательская функция для разворачиваия матрицы коэффициетов различия
 
-euclid_unfold <-    (euclid)
+unfolding <- function(x, method = "euclidean") {
+  n <- nrow(x)
+  N <- (n^2 - n)/2
+  unfold <- data.frame(i = 1:N, Object_j = NA, Object_k = NA, Distance = NA)
+  pos <- 0
+  for(i in 1:(n-1)) for(j in (i+1):n) {
+    pos <- pos + 1
+    unfold$Object_j[pos] <- i
+    unfold$Object_k[pos] <- j
+  }
+  unfold$Distance <- as.vector(vegdist(x, method = method))
+  unfold
+}
 
-bray_unfold  <-    (bray)
 
 
-#Визуализируем соотношение эвклидова расстояния и коэффициента Брея-Куртиса
+disatances <- data.frame(Init =  ,
+                         Stand = ,
+                         Log = ,
+                         Rel = )
 
-Dist <-    (Euclid = euclid_unfold, Bray = bray_unfold)
 
-library(ggplot2)
-
-ggplot(Dist, aes(x = , y = )) +
 
 
 
 # Код для построения частотного распределения расстояний
+library(ggplot2)
+
+
+
+
+## Код для вычисления матрицы расстояния, основанной на разных типах коэффициентов различий
+
+
+
+
+
+
+
+
+
+# Пользовательская функция для Висконсинской полярной ординации
+
+
+polarord <- function(x,...) {
+  dist <- unfolding(x)
+  polar_distance <- max(dist$Distance)
+  Object_polus1 <- dist$Object_j[dist$Distance == max(dist$Distance)]
+  Object_polus2 <- dist$Object_k[dist$Distance == max(dist$Distance)]
+  Polar_coord <- data.frame(Object = 1:nrow(x), Coord = NA)
+  for(i in 1:nrow(x)) {
+
+    AC <- dist$Distance[dist$Object_j == Object_polus1 & dist$Object_k == Object_polus2]
+    if (i != Object_polus1 & i != Object_polus2) BC <- dist$Distance[(dist$Object_j == i & dist$Object_k == Object_polus1)|(dist$Object_j == Object_polus1 & dist$Object_k == i)]
+    if (i != Object_polus1 & i != Object_polus2) AB <- dist$Distance[(dist$Object_j == i & dist$Object_k == Object_polus2)|(dist$Object_j == Object_polus2 & dist$Object_k == i)]
+    if (i != Object_polus1 & i != Object_polus2) Polar_coord$Coord[i] <- (BC^2 + AC^2 - AB^2)/(2 * AC)
+
+  }
+  Polar_coord$Coord[Object_polus1] <- 0
+  Polar_coord$Coord[Object_polus2] <- polar_distance
+  Polar_coord
+}
+
+
+
 
 
 
